99942b7d2ec5bcdb1f0174185948a5dbadb381e4
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2025-09-02 19:34:31 +0100

    test: improve assertion

--- tests/password_validator/password_test.py
+++ tests/password_validator/password_test.py
@@ -7,28 +7,28 @@ from password_validator.password import Password
 
 class PasswordShould(unittest.TestCase):
     def test_be_valid(self):
-        maybe_password: Result[Password, list[str]] = Password.of("Cul4soo!")
+        maybe_password = Password.of("Cul4soo!")
 
         match maybe_password:
             case Success(password):
                 self.assertEqual(str(password), "Cul4soo!")
-            case Failure(_):
-                self.fail("A success was expected")
+            case Failure(errors):
+                self.fail(f"A success was expected but got a failure: {errors}")
 
     def test_require_a_minimum_length(self):
-        maybe_password: Result[Password, list[str]] = Password.of("Cul4so!")
+        maybe_password = Password.of("Cul4so!")
 
         match maybe_password:
             case Success(_):
-                self.fail("A failure was expected")
+                self.fail("A failure was expected but got a success")
             case Failure(errors):
                 self.assertListEqual(["Password must be at least 8 characters long"], errors)
 
     def test_require_at_least_an_uppercase_letter(self):
-        maybe_password: Result[Password, list[str]] = Password.of("cul4soo!")
+        maybe_password = Password.of("cul4soo!")
 
         match maybe_password:
             case Success(_):
-                self.fail("A failure was expected")
+                self.fail("A failure was expected but got a success")
             case Failure(errors):
                 self.assertListEqual(["Password must contain at least one uppercase letter"], errors)
