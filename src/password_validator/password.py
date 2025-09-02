17b797e675a2d68f417df3583bd4cc7f76948954
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-09-02 20:36:12 +0100

    test: green for require a lower case letter, at least one digit and at least a special character

--- src/password_validator/password.py
+++ src/password_validator/password.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from returns.result import Result, Failure, Success
+import re
 
 
 @dataclass
@@ -17,7 +18,12 @@ class Password:
             errors.append("Password must be at least 8 characters long")
         if not any(char.isupper() for char in password):
             errors.append("Password must contain at least one uppercase letter")
-
+        if not any(char.islower() for char in password):
+            errors.append("Password must contain at least one lowercase letter")
+        if not any(char.isdigit() for char in password):
+            errors.append("Password must contain at least one digit")
+        if not re.findall(r'[!*_$&\-;,]+', password):
+            errors.append("Password must contain at least one special character")
         if errors:
             return Failure(errors)
 
